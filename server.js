0eed05f436d1c14656d3cc87f1b697a512d94c73
juliana sousa <sousa_julianacontato@outlook.com>
2024-11-29 01:40:45 -0300

    Commit configuracao cloud google

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
 // Cria uma instância do Express, que será o núcleo da aplicação.
+app.use(express.static("uploads"))
 routes(app) 
 
 app.use(express.json());
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,26 +1,40 @@
 import express from "express";
-import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import multer from "multer";// Configura o armazenamento de arquivos utilizando o Multer
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
-const upload = multer({ dest: "./uploads" , storage: storage})
-//linux ou mac apenas usar a linha a cima
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Define a pasta "uploads/" como destino para os uploads.
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Utiliza o nome original do arquivo para salvá-lo.
+  }
+});
 
+const upload = multer({ storage: storage }); // Configuração para Linux/Mac
+
+// Cria uma instância do Express e define as rotas
 const routes = (app) => {
-    app.use(express.json());
-    // Habilita o middleware para analisar corpos de requisições em formato JSON.
-    app.get("/posts", listarPosts);
-    //Rota para buscar todos os posts 
-    app.post("/posts", postarNovoPost)
-   //Rota para criar tosos os posts
-    app.post("/upload", upload.single("imagem"), uploadImagem)
-}
+  app.use(express.json()); // Habilita o middleware para analisar corpos de requisições em formato JSON.
+  app.use(cors(corsOptions))
+
+  app.get("/posts", listarPosts); // Rota para buscar todos os posts
+  // A função listarPosts (no controller) provavelmente busca todos os posts do banco de dados e retorna os resultados.
+
+  app.post("/posts", postarNovoPost); // Rota para criar um novo post
+  // A função postarNovoPost (no controller) provavelmente recebe os dados do novo post no corpo da requisição e salva no banco de dados.
+
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Rota para upload de imagem
+  // O middleware upload.single("imagem") processa o upload do arquivo "imagem".
+  // A função uploadImagem (no controller) provavelmente salva a imagem e associa ao post.
+
+  app.put("/upload/:id", atualizarNovoPost)
+};
 
 export default routes;
